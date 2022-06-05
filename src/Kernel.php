337e54855c1404084509364dcb95c2988cbfae2c
Magomedcoder <info@magomedcoder.ru>
2022-06-05 20:57:10 +0300

    first commit

---
+++ src/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\HttpServer;
+
+/**
+ * Class Kernel
+ * @package PCore\HttpServer
+ * @github https://github.com/pcore-framework/http-server/src/Kernel.php
+ */
+class Kernel
+{
+
+}
